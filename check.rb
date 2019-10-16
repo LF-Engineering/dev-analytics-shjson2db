a191fb65542aab883b39e07a9b3137e6490a7816
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-10-16 07:33:55 +0000

    Orgs check
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- check.rb
+++ check.rb
@@ -2,8 +2,24 @@
 
 require 'pry'
 require 'json'
+require 'mysql2'
+
+connect = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "root", :database => "merged")
+result = connect.query("select name from organizations")
+eorgs = []
+result.each { |row| eorgs << row['name'] }
+eorgs = eorgs.sort.uniq
 
 o = JSON.parse File.read 'orgs.json'
 bl = o['blacklist']
 orgs = o['organizations'].keys
-binding.pry
+orgs = orgs.sort.uniq
+
+miss = 0
+orgs.each do |org|
+  unless eorgs.include?(org)
+    puts "Missing #{org}" 
+    miss += 1
+  end
+end
+puts "Missing orgs: #{miss}"
